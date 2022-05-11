3ea03c23c73f13a2861a401751ab8cb6d13753db
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2022-05-11 16:15:08 +0530

    Navbar

---
+++ portfolio_deepkshikha/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Home';
+import NavbarTop from './Navbar';
+
+
+
+
+function App() {
+  return (
+      <div>
+        <NavbarTop></NavbarTop>
+        <Home></Home>
+      </div>
+   
+  )
+}
+
+export default App;
---
+++ portfolio_deepkshikha/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';
+function NavbarTop() {
+    return ( 
+        <div>
+        <Navbar collapseOnSelect expand="lg"  >
+        <Navbar.Brand href="#home" >
+            <span>
+               Pegu Deepshikha
+            </span>
+            <span className='brand-inner'>
+                is a narrator of stories
+            </span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            </Nav>
+            <Nav>
+            <Nav.Link href="about" className='navLink' >ABOUT</Nav.Link>
+            <Nav.Link href="culture">CULTURE</Nav.Link>
+            <Nav.Link href="experience">EXPERIENCE DESIGN</Nav.Link>
+            <Nav.Link href="thought">THOUGHT EXPERIMENTS</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+        </div>
+     );
+}
+
+export default NavbarTop;
